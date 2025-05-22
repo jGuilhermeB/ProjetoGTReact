d9f4a50aad773c1dff12f1fb198f29eac7768001
Loboartu <arturllobo@gmail.com>
2025-05-22 11:58:20 -0300

    atualizando o reader em react

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Logo } from './Logo'
+import { SearchField } from './SearchField'
+import { Sing } from './Sing'
+import { Cart } from './Cart'
+import { Navigator } from './Navigator'
 
 const Header = () => {
+
+  const [searchInput, setSearchInput] = useState('')
   return (
-    <div>Header</div>
+    <header className='bg-white'>
+      <div className='w-[86%] mx-auto'>
+        <div className="flex justify-between w-full gap-4 items-center">
+          <Logo/>
+          <SearchField searchInput={searchInput} setSearchInput={setSearchInput}/>
+          <Sing/>
+          <Cart/>
+        </div>
+        <Navigator/>
+      </div>
+
+    </header>
   )
 }
 
