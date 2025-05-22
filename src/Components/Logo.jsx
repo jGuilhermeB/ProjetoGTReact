d33ba4b15b77e593c5c5c9d43d440f6170cc9574
unknown <arturlobo0522@digitalcollege.internal>
2025-05-22 16:30:53 -0300

    header

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import LogoPrimary from '../../public/logo-primary.svg';
+import LogoWhite from '../../public/logo-white.svg';
 
-const Logo = () => {
+export const Logo = ({ background = 'white' }) => {
+  const logoSrc = background === 'dark-gray' ? LogoWhite : LogoPrimary;
+
   return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default Logo
+    <Link to={'/'} className="logo flex items-center cursor-pointer">
+      <img src={logoSrc} alt="Logo" className="w-8" />
+      <h1 className={`font-semibold text-4xl ml-1 text-nowrap ${background === 'dark-gray' ? 'text-white' : 'text-primary'}`}>
+        Digital Store
+      </h1>
+    </Link>
+  );
+};
