ac1daad1346c5509edc2ea043febb5283240e85c
“jGuilhermeB” <jdasilveirabarroso@gmail.com>
2025-05-23 09:50:41 -0300

    Salva alterações locais no BuyBox antes do pull

--- src/Components/BuyBox.jsx
+++ src/Components/BuyBox.jsx
@@ -23,9 +23,9 @@ const BuyBox = ({
   return (
     <div className="w-[700px] h-[570px] rounded-sm flex flex-col gap-4 text-[#1F1F1F] p-6 bg-transparent  mt-10">
       {/* Título */}
-      <h1 className="text-2xl font-bold font">{name}</h1>
+      <h1 className="text-2xl font-bold">{name}</h1>
       {/* Categoria e código */}
-      <p className="text-xs text-[#666]"> {reference}</p>
+      <p className="text-xs text-[#CCCCCC]"> {reference}</p>
 
       {/* Estrelas e avaliação */}
       <div className="flex items-center gap-2 text-sm">
@@ -35,12 +35,12 @@ const BuyBox = ({
             <Star
               key={i}
               size={16}
-              fill={i <= 4 ? '#F6AA1C' : 'none'} // 4 amarelas, 1 vazia
+              fill={i <= 4 ? '#F6AA1C' : 'none'} //if para as estrelas
               stroke="#F6AA1C" // borda preta
             />
           ))}
         </div>
-        {/* Valor na caixa amarela depois */}
+        {/* Valor na caixa amarela  */}
         <span className="font-semibold text-white bg-[#F6AA1C] rounded px-2 py-0.5">
           {stars}
         </span>
