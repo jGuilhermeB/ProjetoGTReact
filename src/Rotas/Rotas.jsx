5627981662871bd23db576d9ec590c53acb8893e
“jGuilhermeB” <jdasilveirabarroso@gmail.com>
2025-05-30 13:55:15 -0300

    fix import

--- src/Rotas/Rotas.jsx
+++ src/Rotas/Rotas.jsx
@@ -4,8 +4,8 @@ import HomePage from '../pages/HomePage'
 import ProductListingPage from '../pages/ProductListingPage'
 import ProductViewPage from '../pages/ProductViewPage'
 import Layout from '../Components/Layout'
-import FilterGroup from '../Components/FilterGroup'
 import { ProductProvider } from '../context/CardContext'
+
 
 
 const Rotas = () => {
@@ -17,8 +17,6 @@ const Rotas = () => {
                         <Route index element={<HomePage />} />
                         <Route path='produtos' element={<ProductListingPage />} />
                         <Route path='detalhes/:id' element={<ProductViewPage />} />
-
-
                     </Route>
                 </Routes>
             </BrowserRouter>
